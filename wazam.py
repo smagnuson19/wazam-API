0b25d8eae84ea34ce718a2aa6fdab11f57bf0c6c
kylehalloran <34900220+kylehalloran@users.noreply.github.com>
2019-05-20 17:18:17 -0400

    Update wazam.py

--- wazam.py
+++ wazam.py
@@ -1,3 +1,4 @@
+# using the speech2text and get_song_titles functions to produce a list of three songs from an audio file
 from speech2text import speech2text
 from genius import get_song_titles
 
@@ -6,4 +7,6 @@ def wazam(audio_file):
     return get_song_titles(lyrics)
 
 
+# testing an example audio file
 ex_file = '/Users/kylehalloran/Desktop/College_Park.flac'
+pprint.pprint(wazam(ex_file))
